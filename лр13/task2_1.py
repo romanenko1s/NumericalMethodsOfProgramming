1316cb842b640ad84a75051b3d8ef1004c11813f
Illya <90707406+romanenkois@users.noreply.github.com>
2022-11-18 01:57:47 +0200

    Create task2_1.py

---
+++ лр13/task2_1.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x0 = 0.5
+y0 = 1.8
+x = [x0]
+y = [y0]
+i = 0
+
+while i < 10:
+    x.append(round(x[i] + 0.1, 4))
+    y.append(round((y[i] + 0.05*(x[i] + np.sin(y[i]/1.25) + x[i+1] + np.sin(y[i]/1.25 + 0.1*x[i] + np.sin(y[i]/1.25)))), 4))
+    i+=1
+
+print(x)
+print(y)
+
+plt.plot(x, y)
+plt.grid()
+plt.show()
