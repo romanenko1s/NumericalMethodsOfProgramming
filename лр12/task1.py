e2450fc635a7927f03fd82208496a1c8d5977328
Illya <90707406+romanenkois@users.noreply.github.com>
2022-11-19 19:24:29 +0200

    Create task1.py

---
+++ лр12/task1.py
@@ -0,0 +1,36 @@
+from scipy import integrate 
+import math
+
+eps = 0.001
+def f1(x): 
+    return 1/((12*x+0.5)**(1/2))
+
+def left_rec(f1,a,b,n): 
+    h=(b-a)/n 
+    sum=0 
+    for i in range(0,n): 
+        sum+=f1(a+i*h) 
+    return sum*h 
+
+v,err = integrate.quad(f1,0.6,1.4)
+
+if abs(left_rec(f1,1.2,2,2*10) - left_rec(f1,0.6,1.4,10))/3. <=eps:
+    print("left rectangle:",round (left_rec(f1,0.6,1.4,10), 5)) 
+
+def right_rec(f1,a,b,n): 
+    h=(b-a)/n 
+    sum=0 
+    for i in range(1,n+1): 
+        sum+=f1(a+i*h) 
+    return sum*h 
+print("right rectangle:",round (right_rec(f1,0.6,1.4,10), 5)) 
+
+def aver_rec(f1,a,b,n): 
+    h=0.08 
+    sum=0 
+    for i in range(0,n): 
+        sum+=f1(a+i*h) 
+    return sum*h 
+
+print("average rectangle:",round (aver_rec(f1,0.6,1.4,10), 5)) 
+print("Check for the rectangle method= ",round (v, 5))
