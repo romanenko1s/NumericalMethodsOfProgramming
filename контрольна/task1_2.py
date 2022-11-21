49073b1c503d5f6f519ae58e44c64908b99b40c3
Illya <90707406+romanenkois@users.noreply.github.com>
2022-11-21 15:36:21 +0200

    Create task1_2.py

---
+++ контрольна/task1_2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate as interp
+
+speed = [25, 35, 45, 30, 60, 120, 100, 100, 70, 75, 80, 65]
+time = np.linspace(0, 11, 12)
+
+f = interp.interp1d(time, speed, kind='quadratic')
+time_new = np.linspace(0, 11, 10_000)
+speed_new = f(time_new)
+
+plt.plot(time_new, speed_new)
+plt.plot(time, speed, 'o')
+plt.xlabel('Час (год)')
+plt.ylabel('Швидкість (км/год)')
+plt.xticks(time)
+plt.yticks(np.arange(0, 140, 10))
+plt.grid()
+plt.show()
+
+distance = np.trapz(speed_new, time_new)
+print(f'Шлях: {distance:.3f} км')
